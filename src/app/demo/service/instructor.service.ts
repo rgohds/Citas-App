95a18b9666703a5f6f51f18d0e9c95de339f4d0a
rgohds <127057257+rgohds@users.noreply.github.com>
2024-04-12 21:31:27 -0600

    Modificar Front End y Utilizar el API Rest de Instructores, correcciones en componente por nombre de campos

--- src/app/demo/service/instructor.service.ts
+++ src/app/demo/service/instructor.service.ts
@@ -1,17 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Instructor } from '../api/instructor';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs';
+
 
 @Injectable()
 export class InstructorService {
 
     constructor(private http: HttpClient) { }
 
-    getInstructores() {
-        return this.http.get<any>('assets/demo/data/instructores.json')
-            .toPromise()
-            .then(res => res.data as Instructor[])
-            .then(data => data);
+    getInstructores(): Observable<Instructor[]> {
+
+        return this.http.get('http://localhost:8080/api/instructores').pipe(
+            map( (response) => response as Instructor[] )
+        );
+
     }    
     
 }
